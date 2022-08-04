58f91f5c784710fcde05e2c1f98be95cd749a9f5
30p87 <marc.ian.straube@gmail.com>
2022-08-04 22:13:07 +0200

    Wolfes dont attack

--- src/main/java/de/_30p87/privatefixes/WolfesDontAttack.java
+++ src/main/java/de/_30p87/privatefixes/WolfesDontAttack.java
@@ -2,19 +2,17 @@ package de._30p87.privatefixes;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.passive.EntityWolf;
-import net.minecraft.world.World;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
-
-import static de._30p87.privatefixes.PrivateFixes.LOGGER;
 
 @Mod.EventBusSubscriber
 public class WolfesDontAttack {
     @SubscribeEvent
     public static void onTick(TickEvent.WorldTickEvent event) {
         for (Entity entity : event.world.loadedEntityList) {
-            if (entity instanceof EntityWolf && ((EntityWolf) entity).isTamed()) LOGGER.info(((EntityWolf) entity).getAttackTarget());
+            if (entity instanceof EntityWolf && ((EntityWolf) entity).isTamed() && ((EntityWolf) entity).getAttackTarget() instanceof EntityPlayer) ((EntityWolf) entity).setAttackTarget(null);
         }
     }
 }
